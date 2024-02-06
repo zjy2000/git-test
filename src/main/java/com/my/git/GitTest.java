00e50abcf0099d6db719be16df1ad90aea74b8c9
zjy <940771376@qq.com>
2024-02-06 16:29:12 +0800

    second commit

--- src/main/java/com/my/git/GitTest.java
+++ src/main/java/com/my/git/GitTest.java
@@ -3,5 +3,6 @@ package com.my.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world222");
     }
 }
